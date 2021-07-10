34b08c5e429bead343b9207e571861dc3d47cd42
sheraznabimirza <sheraznabimirza@gmail.com>
2021-07-11 00:54:05 +0500

    layout with minimal interactivity

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import {
+  FaArrowDown,
+  FaArrowUp,
+  FaPause,
+  FaPlay,
+  FaSyncAlt,
+} from "react-icons/fa";
 function App() {
+  const [breakLength, setBreakLength] = useState(5);
+  const [sessionLength, setSessionLength] = useState(25);
+
+  const handleSessionUp = () => {
+    if (sessionLength < 60) {
+      setSessionLength(sessionLength + 1);
+    }
+  };
+
+  const handleSessionDown = () => {
+    if (sessionLength > 1) {
+      setSessionLength(sessionLength - 1);
+    }
+  };
+
+  const handleBreakUp = () => {
+    if (breakLength < 60) {
+      setBreakLength(breakLength + 1);
+    }
+  };
+
+  const handleBreakDown = () => {
+    if (breakLength > 1) {
+      setBreakLength(breakLength - 1);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id="main-title">Pomodoro Clock</h1>
+      <div className="labels">
+        <div className="break-session">
+          <h3 id="break-label">Break Length</h3>
+          <div className="seedha">
+            <FaArrowDown className="aruuba" onClick={handleBreakDown} />
+            <div className="aruuba nom">{breakLength}</div>
+            <FaArrowUp className="aruuba" onClick={handleBreakUp} />
+          </div>
+        </div>
+        <div className="break-session">
+          <h3 id="session-label">Session Length</h3>
+          <div className="seedha">
+            <FaArrowDown className="aruuba" onClick={handleSessionDown} />
+            <div className="aruuba nom">{sessionLength}</div>
+            <FaArrowUp className="aruuba" onClick={handleSessionUp} />
+          </div>
+        </div>
+      </div>
+      <div className="seedha">
+        <div className="timer">
+          <div id="timer-label">Session</div>
+          <div id="time-left">01:00</div>
+        </div>
+      </div>
+      <div className="timer-control seedha">
+        <FaPlay />
+        <FaPause />
+        <FaSyncAlt />
+      </div>
     </div>
   );
 }
